763a48a51c00df484ba91effca52e0186bce7b4b
Luis Felipe Bertel <ellipe@gmail.com>
2021-07-31 21:02:45 -0500

    fixed image

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,8 @@ export default function App() {
         <div className="px-8 py-12 max-w-md mx-auto sm:max-w-xl lg:px-12 lg:py-24 lg:max-w-full xl:mr-0 2xl:col-span-2">
           <div className="xl:max-w-xl">
             <img className="h-10" src="/img/logo.svg" alt="Workcation" />
-            <img
-              className="mt-6 rounded-lg shadow-xl sm:mt-8 sm:h-64 sm:w-full sm:object-cover object-center lg:hidden"
-              src="/img/beach-work.jpeg"
+            <img className="mt-6 rounded-lg shadow-xl sm:mt-8 sm:h-64 sm:w-full sm:object-cover object-center lg:hidden"
+              src="/img/beach-work.jpg"
               alt="Woman workcationing on the beach"
             />
             <h1 className="mt-6 text-2xl font-bold text-gray-900 sm:mt-8 sm:text-4xl lg:text-3xl xl:text-4xl">
